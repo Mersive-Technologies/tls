a5681d170564f2221d29f6fe820dd1dc83843216
Brent Gardner <bgardner@squarelabs.net>
2021-01-26 12:42:16 -0700

    async

--- tokio-rustls/examples/client/src/main.rs
+++ tokio-rustls/examples/client/src/main.rs
@@ -8,6 +8,8 @@ use std::sync::Arc;
 use tokio::io::{copy, split, stdin as tokio_stdin, stdout as tokio_stdout, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio_rustls::{rustls::ClientConfig, webpki::DNSNameRef, TlsConnector};
+use tokio::sync::oneshot;
+use tokio::time::Duration;
 
 /// Tokio Rustls client example
 #[derive(FromArgs)]
@@ -56,8 +58,7 @@ async fn main() -> io::Result<()> {
         .to_socket_addrs()?
         .next()
         .ok_or_else(|| io::Error::from(io::ErrorKind::NotFound))?;
-    let domain = options.domain.unwrap_or(options.host);
-    let content = format!("GET / HTTP/1.0\r\nHost: {}\r\n\r\n", domain);
+    let domain1 = options.domain.unwrap_or(options.host);
 
     let mut config = ClientConfig::new();
     config.dangerous()
@@ -79,15 +80,25 @@ async fn main() -> io::Result<()> {
 
     let (mut stdin, mut stdout) = (tokio_stdin(), tokio_stdout());
 
-    let domain = DNSNameRef::try_from_ascii_str(&domain)
+    let domain = DNSNameRef::try_from_ascii_str(&domain1)
         .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid dnsname"))?;
 
     let mut stream = connector.connect(domain, stream).await?;
-    stream.write_all(content.as_bytes()).await?;
+    // stream.write_all(content.as_bytes()).await?;
 
     let (mut reader, mut writer) = split(stream);
 
+    let (tx1, rx1) = oneshot::channel();
+    std::thread::spawn(move || {
+        std::thread::sleep(Duration::from_millis(1000));
+        let content = format!("GET / HTTP/1.0\r\nHost: {}\r\n\r\n", domain1);
+        tx1.send(content);
+    });
+
     tokio::select! {
+        val = rx1 => {
+            writer.write_all(val.unwrap().as_bytes()).await?;
+        }
         ret = copy(&mut reader, &mut stdout) => {
             ret?;
         },
